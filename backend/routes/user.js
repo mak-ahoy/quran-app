fb30592a5664d6b7e9c5365c4c6689f05cbb97c9
Ali Haris <ali.haris@sixlogics.local>
2024-07-04 14:46:10 +0500

    first commit

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { app } from "../server.js";
+import { loginUser, registerUser } from "../controllers/user.js";
+import { getSurahVerses, getSurahs } from "../controllers/content.js";
+
+export const userRouter = express.Router();
+
+userRouter.post('/login', loginUser);
+userRouter.post('/register', registerUser);
+userRouter.get('/get-surahs', getSurahs);
+userRouter.post('/get-surahs/surah-info', getSurahVerses);
+
+
